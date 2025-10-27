bb980a943adafc2f527850905a9c3b810207bedd
prevrod-it <it@prevrod.com>
2025-10-27 11:44:36 +0000

    Backend login page demo advertising

--- login.php
+++ login.php
@@ -5,6 +5,9 @@
             <form method="post" action="/admin" role="login" enctype="multipart/form-data">
                 <img src="<?php echo $core_elements->tree_struct()->mediadir->adminimg; ?>/top_navbar_logo.png" class="img-fluid mb-4" alt="E-ROD">
                 <h3 class="pb-3">Portal de administração</h3>
+                <div class="alert alert-primary py-2 mb-3 text-center">
+                    <i class="fas fa-exclamation-triangle pe-2"></i><span class="fw-bold">VERSÃO DE DEMONSTRAÇÃO E FORMAÇÃO</span><i class="fas fa-exclamation-triangle ps-2"></i>
+                </div>
                 <input type="text" name="usr" class="form-control mb-3" id="usr" placeholder="e-mail" value="<?php echo $usr; ?>" autocomplete="on"<?php echo ($usr == "") ? " autofocus" : ""; ?>>         
                 <input type="password" name="pwd" class="form-control mb-3" id="pwd" placeholder="Password" autocomplete="on"<?php echo ($usr == "") ? "" : " autofocus"; ?>>
                 <input type="hidden" name="vardumb" id="vardumb" value="!ghUT98B%09fDe#5j7G%dYU7ap8_Bd7Hw">
